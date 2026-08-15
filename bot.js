4fe486f36dfd332ccafdb209d1a880e09e1b2e9c
teoxyrx9pro-stack <teoxyrx9pro@gmail.com>
2026-08-15 16:30:59 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,32 @@
+const mineflayer = require('mineflayer');
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'MCSharkSMP.aternos.me',
+    port: 59439,
+    username: 'SharkSMPBoT',
+    version: '1.21.4',
+    auth: 'offline'
+  });
+
+  bot.on('spawn', () => {
+    console.log('Bot je uspješno ušao na server!');
+
+    setInterval(() => {
+      bot.setControlState('jump', true);
+      setTimeout(() => bot.setControlState('jump', false), 500);
+    }, 30000);
+  });
+
+  bot.on('end', (reason) => {
+    console.log('Razlog izbacivanja:', reason);
+    console.log('Pokušavam ponovno spajanje za 5 sekundi...');
+    setTimeout(createBot, 5000);
+  });
+
+  bot.on('error', (err) => {
+    console.log('Tehnička greška:', err.message);
+  });
+}
+
+createBot();
